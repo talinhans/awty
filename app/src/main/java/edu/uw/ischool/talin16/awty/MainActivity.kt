c50317e6ff3ee671d8eeee02cd38dad369764c2c
talinhans <hans.talin@gmail.com>
2022-05-06 22:56:24 +0530

    cleanup

--- app/src/main/java/edu/uw/ischool/talin16/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/talin16/awty/MainActivity.kt
@@ -2,7 +2,6 @@ package edu.uw.ischool.talin16.awty
 
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -27,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         btnStart.setOnClickListener {
             if (toggle) {
                 validateInputAndStart(
-                    etMessage.text.toString(),
+                    etMessage.text.toString().trim(),
                     etPhoneNumber.text.toString().trim(),
                     etTime.text.toString().trim()
                 )
@@ -44,37 +43,51 @@ class MainActivity : AppCompatActivity() {
         etPhoneNumber: String?,
         etTimeString: String?
     ) {
-        if (etTimeString.isNullOrEmpty()){
-            Toast.makeText(baseContext,"Cannot start because time field is empty " ,Toast.LENGTH_SHORT).show()
+        if (etTimeString.isNullOrEmpty()) {
+            Toast.makeText(
+                baseContext,
+                "Cannot start because time field is empty ",
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
-        var etTime=etTimeString?.toInt()
+        var etTime = etTimeString?.toInt()
 
         if (etTime != null) {
             if (etTime <= 0) {
-                Toast.makeText(baseContext,"Cannot start because time is equal to or less than 0 " ,Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    baseContext,
+                    "Cannot start because time is equal to or less than 0 ",
+                    Toast.LENGTH_SHORT
+                ).show()
                 return
             }
         } else {
-            Toast.makeText(baseContext,"Cannot start because time is null " ,Toast.LENGTH_SHORT).show()
+            Toast.makeText(baseContext, "Cannot start because time is null ", Toast.LENGTH_SHORT)
+                .show()
             return
         }
         if (etPhoneNumber?.length != 10) {
-            Toast.makeText(baseContext,"Cannot start because digits in phone number are less than 10 " ,Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                baseContext,
+                "Cannot start because digits in phone number are not 10 ",
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
         if (etMessage.isNullOrEmpty()) {
-            Toast.makeText(baseContext,"Cannot start because message is empty" ,Toast.LENGTH_SHORT).show()
+            Toast.makeText(baseContext, "Cannot start because message is empty", Toast.LENGTH_SHORT)
+                .show()
             return
         }
 
         btnStart.text = "Stop"
-        toggle=false
+        toggle = false
 
         val delay =1000*etTime*60// 1000 milliseconds == 1 second
         handler.postDelayed(object : Runnable {
             override fun run() {
-                Toast.makeText(baseContext, etMessage, Toast.LENGTH_SHORT).show()
+                Toast.makeText(baseContext, etMessage.trim(), Toast.LENGTH_SHORT).show()
                 handler.postDelayed(this, delay.toLong())
             }
         }, delay.toLong())
