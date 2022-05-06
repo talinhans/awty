d1323758f4e28e92c3e66491e939fdd89cb717aa
talinhans <hans.talin@gmail.com>
2022-05-06 18:19:25 +0530

    part-2 Init

--- app/src/main/java/edu/uw/ischool/talin16/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/talin16/awty/MainActivity.kt
@@ -1,7 +1,10 @@
 package edu.uw.ischool.talin16.awty
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -17,7 +20,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         var etMessage = findViewById<EditText>(R.id.etMessage)
         var etPhoneNumber = findViewById<EditText>(R.id.etPhoneNumber)
         var etTime = findViewById<EditText>(R.id.etTime)
@@ -44,41 +46,75 @@ class MainActivity : AppCompatActivity() {
         etPhoneNumber: String?,
         etTimeString: String?
     ) {
-        if (etTimeString.isNullOrEmpty()){
-            Toast.makeText(baseContext,"Cannot start because time field is empty " ,Toast.LENGTH_SHORT).show()
+        if (etTimeString.isNullOrEmpty()) {
+            Toast.makeText(
+                baseContext,
+                "Cannot start because time field is empty ",
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
-        var etTime=etTimeString?.toInt()
+        var etTime = etTimeString?.toInt()
 
         if (etTime != null) {
             if (etTime <= 0) {
-                Toast.makeText(baseContext,"Cannot start because time is equal to or less than 0 " ,Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    baseContext, "Cannot start because time is equal to or less than 0 ",
+                    Toast.LENGTH_SHORT
+                ).show()
                 return
             }
         } else {
-            Toast.makeText(baseContext,"Cannot start because time is null " ,Toast.LENGTH_SHORT).show()
+            Toast.makeText(baseContext, "Cannot start because time is null ", Toast.LENGTH_SHORT)
+                .show()
             return
         }
         if (etPhoneNumber?.length != 10) {
-            Toast.makeText(baseContext,"Cannot start because digits in phone number are less than 10 " ,Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                baseContext, "Cannot start because digits in phone number are less than 10 ",
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
         if (etMessage.isNullOrEmpty()) {
-            Toast.makeText(baseContext,"Cannot start because message is empty" ,Toast.LENGTH_SHORT).show()
+            Toast.makeText(baseContext, "Cannot start because message is empty", Toast.LENGTH_SHORT)
+                .show()
             return
         }
 
         btnStart.text = "Stop"
-        toggle=false
+        toggle = false
 
-        val delay =1000*etTime*60// 1000 milliseconds == 1 second
+        val delay = 1000 * etTime * 60// 1000 milliseconds == 1 second
+        //val delay = 2000
         handler.postDelayed(object : Runnable {
             override fun run() {
-                Toast.makeText(baseContext, etMessage, Toast.LENGTH_SHORT).show()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    //Log.d("Current", "Inside Permission Check")
+                    if (checkSelfPermission(android.Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                        try {
+                            val smsManager: SmsManager = SmsManager.getDefault()
+                            smsManager.sendTextMessage(
+                                etPhoneNumber,
+                                null,
+                                etMessage,
+                                null,
+                                null
+                            ) // this will send u the message
+
+                            // Toast.makeText(baseContext, "Message Send", Toast.LENGTH_SHORT).show()
+
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            //   Log.d("Current", e.message.toString())
+                        }
+                    } else {
+                        requestPermissions(arrayOf(android.Manifest.permission.SEND_SMS), 1)
+                    }
+                }
                 handler.postDelayed(this, delay.toLong())
             }
         }, delay.toLong())
     }
-
 
 }
